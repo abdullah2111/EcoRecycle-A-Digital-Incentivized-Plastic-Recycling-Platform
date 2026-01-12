7cdfd11030890aaf57368cf59365546e11008e67
Abdullah Al Mamun <abdullah.official2111@gmail.com>
2026-01-12 11:16:14 +0600

    Initial Project setup with Initial dependency injection

---
+++ src/main/java/com/example/ecorecycle/EcoRecycleApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecorecycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoRecycleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoRecycleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecorecycle/EcoRecycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecorecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoRecycleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
